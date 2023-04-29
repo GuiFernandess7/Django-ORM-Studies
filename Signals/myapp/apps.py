46c96281ac9a10f557e96fdd7a5c46112264405a
Guilherme Fernandes <guilhermefs09@protonmail.com>
2023-04-29 16:01:31 -0300

    basic signal added

--- Signals/myapp/apps.py
+++ Signals/myapp/apps.py
@@ -1,6 +1,12 @@
 from django.apps import AppConfig
+from django.db.models.signals import post_save
 
+def example_receiver(sender, **kwargs):
+    print("Intance is saved")
 
 class MyappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'myapp'
+
+    def ready(self) -> None:
+        post_save.connect(example_receiver)
