8978755cf0b47378860c3adb371f0d1db29f6741
Guilherme Fernandes <guilhermefs09@protonmail.com>
2023-04-30 17:17:59 -0300

    Category_Product inline created

--- ManyToManyProj/inventory/admin.py
+++ ManyToManyProj/inventory/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Category, Product, Product_Category
 
-# Register your models here.
+admin.site.register(Product_Category)
+
+class CategoryInline(admin.TabularInline):
+    model = Product.category.through
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {
+        "slug": ("name",)
+    }
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    prepopulated_fields = {
+        "slug": ("name",)
+    }
+    inlines = [CategoryInline]
+
