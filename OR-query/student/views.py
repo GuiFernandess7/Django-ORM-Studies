21d26d18b35908bc8db4297000f02e546abce869
Guilherme Fernandes <guilhermefs09@protonmail.com>
2023-05-07 18:48:59 -0300

    Union added

--- OR-query/student/views.py
+++ OR-query/student/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
-from .models import Student
+from .models import Student, Teacher
 from django.db import connection
 from django.db.models import Q
 
-# Part 2
+# Part 2 - OR
 #################################################################
 def student_list_(request):
 
@@ -32,7 +32,7 @@ def student_list_(request):
 
     return render(request, 'output.html',{'posts':posts})
 
-# Part 3
+# Part 3 - AND
 #####################
 
 def student_list_(request):
@@ -44,11 +44,24 @@ def student_list_(request):
 
     return render(request, 'output.html',{'posts':posts})
 
-def student_list(request):
+def student_list_(request):
     posts = Student.objects.filter(Q(surname__startswith='baldwin') &
                                    Q(firstname__startswith='lakisha'))
     
     print(posts)
     print(connection.queries)
 
+    return render(request, 'output.html', {'posts':posts})
+
+# Part 4 - UNION
+#####################
+
+def student_list(request):
+
+    posts = Student.objects.all().values("firstname").union(
+        Teacher.objects.all().values("firstname")
+    )
+
+    print(posts)
+    print(connection.queries)
     return render(request, 'output.html', {'posts':posts})
