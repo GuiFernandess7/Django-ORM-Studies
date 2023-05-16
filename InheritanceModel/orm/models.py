77d34c7da4d05c7b6a3aeae8d5be9e55386da0aa
Guilherme Fernandes <guilhermefs09@protonmail.com>
2023-05-16 20:33:23 -0300

    Multi Table Model added

--- InheritanceModel/orm/models.py
+++ InheritanceModel/orm/models.py
@@ -1,8 +1,21 @@
 from django.db import models
 from django.utils import timezone
 
-# Abstract Model
+# Multi Table Model
+class Books(models.Model):
+    title = models.CharField(max_length=100)
+    created = models.DateTimeField(auto_now_add=True)
 
+class ISBN(models.Model):
+    # Is generated automatically
+    books_ptr = models.OneToOneField(
+        Books, on_delete=models.CASCADE,
+        parent_link=True,
+        primary_key=True)
+    ISBN = models.TextField()
+    detail = models.TextField(blank=True)
+
+# Abstract Model
 class BaseItem(models.Model):
     title = models.CharField(max_length=255)
     created = models.DateTimeField(auto_now_add=True)
